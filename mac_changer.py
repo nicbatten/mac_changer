4322a5028cfb92da10f6ed6513436eb65d881ee1
nicbatten <48190415+nicbatten@users.noreply.github.com>
2019-08-06 08:31:14 -0400

    Changed to accept user input

--- mac_changer.py
+++ mac_changer.py
@@ -2,11 +2,11 @@
 
 import subprocess
 
-interface = "wlan0"
-new_mac="00:11:22:33:44:77"
+interface = input("interface > ")
+new_mac=input("new MAC > ")
 
 print("[+] changing MAC address for " + interface + " to " + new_mac)
 
-#subprocess.call("ifconfig eth0 down", shell=True)
-#subprocess.call("ifconfig eth0 hw ether 11:22:33:44:55:66", shell=True)
-#subprocess.call("ifconfig eth0 up", shell=True)
+subprocess.call("ifconfig " + interface + " down", shell=True)
+subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell=True)
+subprocess.call("ifconfig " + interface + " up", shell=True)
