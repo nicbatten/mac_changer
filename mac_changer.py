f862736ccdb047e7c8052fef24c078da2dce5abd
nicbatten <48190415+nicbatten@users.noreply.github.com>
2019-08-06 08:48:11 -0400

    Changed how we handle the subprocess.call input

--- mac_changer.py
+++ mac_changer.py
@@ -5,8 +5,8 @@ import subprocess
 interface = input("interface > ")
 new_mac=input("new MAC > ")
 
-print("[+] changing MAC address for " + interface + " to " + new_mac)
+print("[+] Changing MAC address for " + interface + " to " + new_mac)
 
-subprocess.call("ifconfig " + interface + " down", shell=True)
-subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell=True)
-subprocess.call("ifconfig " + interface + " up", shell=True)
+subprocess.call(["ifconfig", interface, "down"])
+subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+subprocess.call(["ifconfig", interface, "up"])
