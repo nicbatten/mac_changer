9917f1e4e9aa48f1d4657feaa31696aa35e9ac83
nicbatten <48190415+nicbatten@users.noreply.github.com>
2019-08-05 23:39:16 -0400

    Adding more features

--- mac_changer.py
+++ mac_changer.py
@@ -1,5 +1,7 @@
-#1/usr/bin/env python
+#!/usr/bin/env python
 
 import subprocess
 
-subprocess.call("ifconfig",shell=True)
+subprocess.call("ifconfig eth0 down", shell=True)
+subprocess.call("ifconfig eth0 hw ether 11:22:33:44:55:66", shell=True)
+subprocess.call("ifconfig eth0 up", shell=True)
