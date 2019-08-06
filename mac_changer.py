aad0a717ffb8793b4a2fdbb1b4c99b12673e8855
nicbatten <48190415+nicbatten@users.noreply.github.com>
2019-08-05 23:51:51 -0400

    Adding variables to get ready to accept user input

--- mac_changer.py
+++ mac_changer.py
@@ -2,6 +2,11 @@
 
 import subprocess
 
-subprocess.call("ifconfig eth0 down", shell=True)
-subprocess.call("ifconfig eth0 hw ether 11:22:33:44:55:66", shell=True)
-subprocess.call("ifconfig eth0 up", shell=True)
+interface = "wlan0"
+new_mac="00:11:22:33:44:77"
+
+print("[+] changing MAC address for " + interface + " to " + new_mac)
+
+#subprocess.call("ifconfig eth0 down", shell=True)
+#subprocess.call("ifconfig eth0 hw ether 11:22:33:44:55:66", shell=True)
+#subprocess.call("ifconfig eth0 up", shell=True)
